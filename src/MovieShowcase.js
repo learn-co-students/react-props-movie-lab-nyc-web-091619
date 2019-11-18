fc1ddb520f45ea5fc00098ce20be4beb546a92e6
H. Turcan Cicek <htcicek@users.noreply.github.com>
2019-11-18 13:33:20 -0500

    met all specs

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
-import MovieCard from './card-components/MovieCard.js'
-import movieData from './data.js'
+import React, { Component } from "react";
+import MovieCard from "./card-components/MovieCard.js";
+import movieData from "./data.js";
 
 export default class MovieShowcase extends Component {
-
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
-  }
+    return movieData.map(obj => {
+      return (
+        <MovieCard
+          title={obj.title}
+          IMDBRating={obj.IMDBRating}
+          genres={obj.genres}
+          poster={obj.poster}
+        />
+      );
+    });
+  };
 
   render() {
-    return (
-      <div id="movie-showcase">
-        {this.generateMovieCards()}
-      </div>
-    )
+    return <div id="movie-showcase">{this.generateMovieCards()}</div>;
   }
 }
